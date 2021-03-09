44aa254b704531501d9cda5f79ae1a8fec701670
Shubham Choudhary <work.shubhamchoudhary@gmail.com>
2021-03-09 16:04:45 +0530

    refactored components and used props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,33 @@ import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 import "./App.css";
 
+let business = {
+	imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+	name: "MarginOtto Pizzeria",
+	address: "1010 Paddington Way",
+	city: "Flavortown",
+	state: "NY",
+	zipCode: " 10101",
+	category: "Italian",
+	rating: 4.5,
+	reviewCount: 90,
+};
+
+const businessList = [
+	business,
+	business,
+	business,
+	business,
+	business,
+	business,
+];
+
 function App() {
 	return (
 		<div className="App">
 			<h1>foodie</h1>
 			<SearchBar />
-			<BusinessList />
+			<BusinessList businesses={businessList} />
 		</div>
 	);
 }
